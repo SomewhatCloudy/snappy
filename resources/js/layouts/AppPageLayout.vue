0c080e425624f860cc1ed31e3594fe20d9a596b0
Andrew Laignel <andrew@laignel.com>
2026-03-04 14:02:53 +0000

    Admin login instructions

--- resources/js/layouts/AppPageLayout.vue
+++ resources/js/layouts/AppPageLayout.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { index } from '@/routes/admin/stores';
+import { Link } from '@inertiajs/vue3';
 
 </script>
 
 <template>
-    <div class="container mx-auto px-4 py-8">
-        <slot />
+    <div class="flex flex-col min-h-[90vh] container mx-auto px-4 py-8">
+        <div class="grow">
+            <slot />
+        </div>
+        <div class="text-center">
+            <Link :href="index.url()" class="inline-block bg-primary text-white px-4 py-2 mb-5 rounded-md">Admin Area</Link>
+            <p class="font-mono text-sm">Authentication: <span class="text-gray-600">admin@example.org / password</span></p>
+        </div>
     </div>
 </template>
